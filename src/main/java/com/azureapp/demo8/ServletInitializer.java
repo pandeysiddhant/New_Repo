d24e37b93363bfd09d3246b6ff0ca21853e118f4
pandeysiddhant <pandeysiddhant85@gmail.com>
2022-05-09 16:08:03 +0530

    first commit

---
+++ src/main/java/com/azureapp/demo8/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.azureapp.demo8;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Demo8Application.class);
+	}
+
+}
